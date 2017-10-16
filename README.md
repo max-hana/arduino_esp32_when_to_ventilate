017f824e7d633364c60681ac5d5efe87d1e63143
max-hana <masa@hanada.net>
2017-10-17 01:03:16 +0900

    CO2 is shown in ppm periodically

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# arduino_esp32_when_to_ventilate
+# arduino_esp32_when_to_ventilate (still in progress)
 
 ![overview](./png/when2ventilate_all.png "overview")
 
-- This is a project for ESP32 with SSD1306 OLED modules.
-- MH-Z19B is required to build this hardware. (get it from ebay or amazon approximately US$20.)
-- MH-Z19B can be connected to microcontroller in two way. PWM or Serial. I chose Serial.
+- This is a project for ESP32 with SSD1306 OLED modules(Heltec WIFI_KIT_32).
+- MH-Z19B(CO2 sensor) is required to build this whole project. (get it from ebay or amazon approximately US$20.)
+- MH-Z19B can be connected to micro controller in two way. PWM or Serial. I chose Serial.
+
+|IO pin | Selected feature |
+|-------|-----------|
+|IO16 | U2_RXD <br />(* OLED reset pin is overridden with U2_RXD)|
+|IO17 |  U2_TXD |
 
 
 ## installation instruction
@@ -17,9 +22,11 @@
 - Add "Adafruit_SSD1306" to your libraries folder.
 [github.com/adafruit/Adafruit_SSD1306](https://github.com/adafruit/Adafruit_SSD1306)
 
-- Due to the nature of Adafruit library, you need to change the value of "SSD1306_LCDHEIGHT" to 64 so that it fits the actual display dimensions.
+- Due to the nature of Adafruit library, you need to do the following:
+1. change the value of "SSD1306_LCDHEIGHT" to 64 so that it fits the display dimensions. To do this, simply uncomment "#define SSD1306_128_64" in stead of "#define SSD1306_128_32" in Adafruit_SSD1306.h:Line74-75
+2. change "Wire.begin()" to "Wire.begin(4,15)" in Adafruit_SSD1306.cpp:Line206
 
 
 ## Notes
 - The ESP32 board has a cheap/low-quality Chinese chip(CH341). This chip cannot go beyond 115200bps.
-- IO pins for Serial1 is already been used. Serial2 is the only available option to communicate with MH-19B for this board.
+- IO pins for Serial1 is not available. Serial2 is the only available option to communicate with MH-19B for this board.
--- arduino_esp32_when_to_ventilate.ino
+++ arduino_esp32_when_to_ventilate.ino
@@ -21,8 +21,11 @@ All text above, and the splash screen must be included in any redistribution
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+
 #define OLED_RESET 16 // 4
 Adafruit_SSD1306 display(OLED_RESET);
+
+HardwareSerial Serial2(2);
 
 
 #define NUMFLAKES 10
@@ -56,16 +59,9 @@ static const unsigned char PROGMEM logo16_glcd_bmp[] =
 #endif
 
 void setup()   {
-//  pinMode(16,OUTPUT);
-//  digitalWrite(16, LOW); // set GPIO16 low to reset OLED
-//  delay(150);
-//  digitalWrite(16, HIGH);
-
-  //Wire.begin(4,15);
-                
-  Serial.begin(9600);
-
-  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  //Wire.begin(4,15);     
+  Serial.begin(115200);
+  
   // display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D (for the 128x64)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C, 1);  // initialize with the I2C addr 0x3D (for the 128x64)
   // init done
@@ -80,312 +76,98 @@ void setup()   {
   Serial.println("clearDisplay();");
   display.clearDisplay();
 
-  // draw a single pixel
-  Serial.println("drawPixel(10, 10, WHITE);");
-  display.drawPixel(10, 10, WHITE);
-  // Show the display buffer on the hardware.
-  // NOTE: You _must_ call display after making any drawing commands
-  // to make them visible on the display hardware!
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-
-  // draw many lines
-  Serial.println("testdrawline();");
-  testdrawline();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw rectangles
-  Serial.println("testdrawrect();");
-  testdrawrect();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw multiple rectangles
-  Serial.println("testfillrect(); // multi");
-  testfillrect();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw mulitple circles
-  Serial.println("testdrawcircle(); // multi");
-  testdrawcircle();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw a white circle, 10 pixel radius
-  display.fillCircle(display.width()/2, display.height()/2, 10, WHITE);
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  testdrawroundrect();
-  delay(2000);
-  display.clearDisplay();
-
-  testfillroundrect();
-  delay(2000);
-  display.clearDisplay();
-
-  testdrawtriangle();
-  delay(2000);
-  display.clearDisplay();
-   
-  testfilltriangle();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw the first ~12 characters in the font
-  testdrawchar();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw scrolling text
-  testscrolltext();
-  delay(2000);
-  display.clearDisplay();
-
-  // text display tests
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println("Hello, world!");
-  display.setTextColor(BLACK, WHITE); // 'inverted' text
-  display.println(3.141592);
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.print("0x"); display.println(0xDEADBEEF, HEX);
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // miniature bitmap display
-  display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
-  display.display();
-  delay(1);
-
-  // invert the display
-  display.invertDisplay(true);
-  delay(1000); 
-  display.invertDisplay(false);
-  delay(1000); 
-  display.clearDisplay();
-
-  // draw a bitmap icon and 'animate' movement
-  testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
+  Serial2.begin(9600); // for mh_z19b 
 }
 
+/*
+ * Reply example
+0-1-255  => 0xff
+0-1-134  => 0x86
+0-1-3    => 0x03
+0-1-18   => 0x12
+0-1-66   => 0x42
+0-1-0    
+0-1-0
+0-1-74   => 0x4A
+0-1-217  => 0xD9
+ */
+
+char buf_co2[20];
+int i_co2=0;
+int val_co2=-1;
+
+int fetch_CO2(){
+  static int count = 10;
+  const byte a[] = {0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79 };
+  if (0==i_co2 && 0 > count){
+    Serial2.write(a,9);
+  }
+  if (Serial2.available() > 0) {
+    count = 10;
+    // read the incoming byte:
+    byte in = Serial2.read();
+    buf_co2[i_co2++] = in;
+    if( (1 == i_co2) && (0xff != buf_co2[0]) ){
+      i_co2 = 0;
+    }
+    if( (2 == i_co2) && (0x86 != buf_co2[1]) ){
+      i_co2 = 0;
+    }
+    if( 4 == i_co2 ){
+      val_co2 = buf_co2[2]*256 + buf_co2[3];
+      i_co2 = 0;
+    }
+  }else{
+    --count;
+    delay(100);
+  }
+  return val_co2;
+}
+
+
+void show_CO2(int ppm) {
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.clearDisplay();
+  display.setCursor(0,0);
+
+  display.print("CO2:");
+  display.print(ppm, DEC);
+  display.println("ppm");
+
+  display.display();
+  delay(1);
+}
 
 void loop() {
-  
-}
+  static int loop_count=0;
 
+  if( loop_count < 100 ){
 
-void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-  uint8_t icons[NUMFLAKES][3];
- 
-  // initialize
-  for (uint8_t f=0; f< NUMFLAKES; f++) {
-    icons[f][XPOS] = random(display.width());
-    icons[f][YPOS] = 0;
-    icons[f][DELTAY] = random(5) + 1;
-    
-    Serial.print("x: ");
-    Serial.print(icons[f][XPOS], DEC);
-    Serial.print(" y: ");
-    Serial.print(icons[f][YPOS], DEC);
-    Serial.print(" dy: ");
-    Serial.println(icons[f][DELTAY], DEC);
-  }
-
-  while (1) {
-    // draw each icon
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, WHITE);
+    if( 0 == loop_count ){
+      delay(10);
+    }else if( 30 > loop_count ){
+      fetch_CO2();
+    }else if( 30 == loop_count ){
+      Serial.print("ppm:");
+      Serial.println(val_co2,DEC);
+      delay(20);
+      display.begin(SSD1306_SWITCHCAPVCC, 0x3C, 1);  // initialize with the I2C addr 0x3D (for the 128x64)
+      display.display();
+    }else if( 35 > loop_count ){
+      delay(100);
+    }else{
+      show_CO2(val_co2);
+      delay(100);
     }
-    display.display();
-    delay(200);
-    
-    // then erase it + move it
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, BLACK);
-      // move it
-      icons[f][YPOS] += icons[f][DELTAY];
-      // if its gone, reinit
-      if (icons[f][YPOS] > display.height()) {
-        icons[f][XPOS] = random(display.width());
-        icons[f][YPOS] = 0;
-        icons[f][DELTAY] = random(5) + 1;
-      }
-    }
-   }
+    loop_count++;
+  }else{
+    loop_count = 0;
+    val_co2=-1;
+  }
+
 }
 
 
-void testdrawchar(void) {
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
 
-  for (uint8_t i=0; i < 168; i++) {
-    if (i == '\n') continue;
-    display.write(i);
-    if ((i > 0) && (i % 21 == 0))
-      display.println();
-  }    
-  display.display();
-  delay(1);
-}
 
-void testdrawcircle(void) {
-  for (int16_t i=0; i<display.height(); i+=2) {
-    display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testfillrect(void) {
-  uint8_t color = 1;
-  for (int16_t i=0; i<display.height()/2; i+=3) {
-    // alternate colors
-    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
-    display.display();
-    delay(1);
-    color++;
-  }
-}
-
-void testdrawtriangle(void) {
-  for (int16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
-    display.drawTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i, WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testfilltriangle(void) {
-  uint8_t color = WHITE;
-  for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
-    display.fillTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i, WHITE);
-    if (color == WHITE) color = BLACK;
-    else color = WHITE;
-    display.display();
-    delay(1);
-  }
-}
-
-void testdrawroundrect(void) {
-  for (int16_t i=0; i<display.height()/2-2; i+=2) {
-    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testfillroundrect(void) {
-  uint8_t color = WHITE;
-  for (int16_t i=0; i<display.height()/2-2; i+=2) {
-    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, color);
-    if (color == WHITE) color = BLACK;
-    else color = WHITE;
-    display.display();
-    delay(1);
-  }
-}
-   
-void testdrawrect(void) {
-  for (int16_t i=0; i<display.height()/2; i+=2) {
-    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testdrawline() {  
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, 0, i, display.height()-1, WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(0, 0, display.width()-1, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, display.height()-1, i, 0, WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (int16_t i=display.width()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(display.width()-1, 0, 0, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE); 
-    display.display();
-    delay(1);
-  }
-  delay(250);
-}
-
-void testscrolltext(void) {
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(10,0);
-  display.clearDisplay();
-  display.println("scroll");
-  display.display();
-  delay(1);
- 
-  display.startscrollright(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrollleft(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);    
-  display.startscrolldiagright(0x00, 0x07);
-  delay(2000);
-  display.startscrolldiagleft(0x00, 0x07);
-  delay(2000);
-  display.stopscroll();
-}
